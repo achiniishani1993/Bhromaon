f27f86cd30fb941dffa9e03b3a92d72624c02fc9
achiniishani1993 <achiniishani1993@gmail.com>
2026-03-10 22:17:36 +0100

    add js to scroll screen and changed hover effect

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
-function openNav (){
-    document.getElementById("mobileNav").style.width ="200px";
+function openNav() {
+  document.getElementById("mobileNav").style.width = "200px";
 }
 
 function closeNav() {
-    document.getElementById("mobileNav").style.width = "0";
+  document.getElementById("mobileNav").style.width = "0";
 }
+
+document.getElementById("scrollBtn").addEventListener("click", function () {
+  document.getElementById("sectionOne").scrollIntoView({
+    behavior: "smooth",
+  });
+});
